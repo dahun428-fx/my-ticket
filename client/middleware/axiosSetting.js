c93e1e51f96fcd1a1d4413eb0c1ab486bc4fa14e
**** <*****>
2023-03-21 18:11:55 +0900

    front axios middle ware

---
+++ client/middleware/axiosSetting.js
@@ -0,0 +1,2 @@
+import axios, {HeadersDefaults} from 'axios';
+
--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source:"/hello",
+        destination:"/hi",
+        permanent : false,
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
